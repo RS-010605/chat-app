2ab4c23b333e76f092fb6f7ae988908d6590a50f
Vishwakarma Ramchandra <ramchandravishwakarmayt@gmail.com>
2025-04-16 16:30:35 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+let users = {}; // { socket.id: username }
+
+app.use(express.static('public'));
+
+function broadcastUserList() {
+  const usernames = Object.values(users);
+  io.emit('user list', usernames);
+}
+
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id);
+
+  socket.on('set username', (username) => {
+    users[socket.id] = username;
+    broadcastUserList();
+    console.log(`User set: ${username} (${socket.id})`);
+  });
+
+  socket.on('private message', ({ to, message }) => {
+    const recipientSocketId = Object.keys(users).find(id => users[id] === to);
+    if (recipientSocketId) {
+      io.to(recipientSocketId).emit('private message', {
+        from: users[socket.id],
+        message,
+      });
+    }
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', users[socket.id]);
+    delete users[socket.id];
+    broadcastUserList();
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
