593653dd4049d559db52588a79069ce0946c797f
Ramchandra Vishwakarma <ramchandravishwakarmayt@gmail.com>
2025-05-15 20:57:40 +0530

    Add .gitignore to ignore node_modules and other files

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
-// 🚀 WhatsApp-style Chat App Server (with Typing Indicator)
+// 🚀 WhatsApp-style Chat App Server (with Typing Indicator & MongoDB persistence)
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://RR060805:ShriRam@cluster1.mdf7vhk.mongodb.net/chatapp?retryWrites=true&w=majority&appName=Cluster1')
+const Message = mongoose.model('Message', {
+  from: String,
+  to: String,
+  text: String,
+  time: String,
+});
 
 const app = express();
 const server = http.createServer(app);
@@ -22,10 +31,20 @@ io.on('connection', (socket) => {
   socket.on('set username', (username) => {
     users[socket.id] = username;
     broadcastUserList();
+    socket.emit('username confirmed', username);
     console.log(`User set: ${username} (${socket.id})`);
   });
 
-  socket.on('private message', ({ to, message }) => {
+  socket.on('private message', async ({ to, message }) => {
+    const time = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    // Save to DB
+    await Message.create({
+      from: users[socket.id],
+      to,
+      text: message,
+      time,
+    });
+    // Send to recipient if online
     const recipientSocketId = Object.keys(users).find(id => users[id] === to);
     if (recipientSocketId) {
       io.to(recipientSocketId).emit('private message', {
@@ -33,6 +52,17 @@ io.on('connection', (socket) => {
         message,
       });
     }
+  });
+
+  socket.on('get messages', async ({ withUser }) => {
+    const myName = users[socket.id];
+    const messages = await Message.find({
+      $or: [
+        { from: myName, to: withUser },
+        { from: withUser, to: myName }
+      ]
+    }).sort({ _id: 1 });
+    socket.emit('chat history', messages);
   });
 
   socket.on('typing', ({ to }) => {
